b0a6945ebf32f6e5ca6f398621598bf43c2b1b77
andrewmcodes <am5648@uncw.edu>
2018-07-08 13:27:40 -0400

    style adjustments

--- src/main.js
+++ src/main.js
@@ -21,5 +21,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 3500);
+    setTimeout(carousel, 4500);
 }
