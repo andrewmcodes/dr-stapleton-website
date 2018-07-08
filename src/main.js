63fa53f6e4f8dc975c5ff818585b56aed34aa2ca
andrewmcodes <am5648@uncw.edu>
2018-07-08 14:00:10 -0400

    add in CV, update styles, add footer

--- src/main.js
+++ src/main.js
@@ -21,5 +21,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 4500);
+    setTimeout(carousel, 1000);
 }
