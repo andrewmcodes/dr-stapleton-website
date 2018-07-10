e8bd590472fcdcd3cb4021b56f2dd1fc946ffc1c
andrewmcodes <am5648@uncw.edu>
2018-07-09 21:12:01 -0400

    Fixes #8

--- src/main.js
+++ src/main.js
@@ -21,5 +21,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 1000);
+    setTimeout(carousel, 7000);
 }
