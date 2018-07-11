1aed258abd7f0969696b96400907166a2d57449f
andrewmcodes <am5648@uncw.edu>
2018-07-11 17:43:45 -0400

    fix for #5 and some refactoring

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -16,19 +16,3 @@ document.addEventListener("DOMContentLoaded", function(){
 function toggleMenu() {
   document.getElementById("js-nav-menu").classList.toggle("hidden");
 }
-
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x;
-    x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 7000);
-}
